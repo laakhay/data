c82993d70bded86cce4b6f6eb67968a00a7d4a9f
Sashank Neupane <sashank.neupane@nyu.edu>
2025-10-11 21:12:37 +0545

    feat(models): add FundingRate model and export
    Introduce FundingRate Pydantic model with helpful properties (percentage/annualized), freshness helpers, and to_dict; export in models package; add unit tests.

--- laakhay/data/models/__init__.py
+++ laakhay/data/models/__init__.py
@@ -1,8 +1,9 @@
 """Data models."""
 
 from .candle import Candle
+from .funding_rate import FundingRate
 from .liquidation import Liquidation
 from .open_interest import OpenInterest
 from .symbol import Symbol
 
-__all__ = ["Candle", "Liquidation", "OpenInterest", "Symbol"]
+__all__ = ["Candle", "FundingRate", "Liquidation", "OpenInterest", "Symbol"]
