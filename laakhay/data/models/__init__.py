622241cc35277d029b778b983bb29bf2c3d7307a
Sashank Neupane <sashank.neupane@nyu.edu>
2025-10-11 22:39:49 +0545

    feat(models): add MarkPrice, OrderBook, and Trade models with tests
    Introduce MarkPrice, OrderBook, and Trade Pydantic models with validation and rich helper properties. Add comprehensive unit tests and export them in models package.

--- laakhay/data/models/__init__.py
+++ laakhay/data/models/__init__.py
@@ -3,7 +3,19 @@
 from .candle import Candle
 from .funding_rate import FundingRate
 from .liquidation import Liquidation
+from .mark_price import MarkPrice
 from .open_interest import OpenInterest
+from .order_book import OrderBook
 from .symbol import Symbol
+from .trade import Trade
 
-__all__ = ["Candle", "FundingRate", "Liquidation", "OpenInterest", "Symbol"]
+__all__ = [
+    "Candle",
+    "FundingRate",
+    "Liquidation",
+    "MarkPrice",
+    "OpenInterest",
+    "OrderBook",
+    "Symbol",
+    "Trade",
+]
